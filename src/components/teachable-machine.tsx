8351f678594c620de9e06e1f8562b4b7b0228837
kiurious <kiurinegrao@gmail.com>
2024-03-15 16:35:55 +0200

    Added actual structure for TeachableMachine - video and buttons

--- src/components/teachable-machine.tsx
+++ src/components/teachable-machine.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,18 +7,11 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import "@tensorflow/tfjs";
-import { useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function TeachableMachine() {
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    setLoading(true);
-  };
-
   return (
     <Card className="w-1/2 bg-primary text-white border-none shadow-xl">
       <CardHeader>
@@ -30,39 +22,16 @@ export default function TeachableMachine() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleSubmit}>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Input
-                className="text-black"
-                id="name"
-                placeholder="Start typing..."
-              />
-            </div>
-            <Button
-              className="bg-white text-black hover:text-white"
-              type="submit"
-            >
-              Analyze
-            </Button>
-          </div>
-        </form>
+        <video id="webcam" autoPlay muted></video>
+        <div>
+          <Button>Enable Webcam</Button>
+          <Button>Gather Class 1 Data </Button>
+          <Button>Gather Class 2 Data</Button>
+          <Button>Train & Predict</Button>
+          <Button>Reset</Button>
+        </div>
       </CardContent>
-      <CardFooter className="flex gap-4">
-        {loading ? (
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-        ) : (
-          <div>
-            <p className="capitalize">{}</p>
-          </div>
-        )}
-      </CardFooter>
+      <CardFooter className="flex gap-4"></CardFooter>
     </Card>
   );
 }
